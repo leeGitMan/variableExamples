a009a570a6401af51d84cb81686bcc6d87c7d85c
leeGitMan <gosrod05@gmail.com>
2022-12-07 13:32:32 +0900

    variableExample2

---
+++ src/edu/kh/variable/ex2/PrintExample.java
@@ -0,0 +1,85 @@
+package edu.kh.variable.ex2;
+
+public class PrintExample {
+
+	public static void main(String[] args) {
+		
+		/* 
+		System.out.println(); : 한 줄 출력(출력 후 줄 바꿈 처림 == 개행)
+		System.out.print(); : 단순 출력(개행 안함)
+		두개는 줄 바꿈이라는 차이점이 있지만, 괄호 안에 내용은 그대로 출력된다는 것은 똑같다.
+		
+		*/
+		
+		System.out.println("Test");
+		System.out.println("Test2");
+		System.out.println("Test");
+		System.out.println("Test");
+		System.out.println("Test");
+		System.out.println("Test");
+		System.out.println("Test");
+		System.out.println("Test");
+		System.out.println("Test");
+		System.out.println("Test");
+		System.out.println();
+		
+		System.out.print("Test3");
+		System.out.print("Test4");
+		System.out.print("Test5");
+		System.out.println();
+		System.out.println();
+		
+		// System.out.printf(); : 출력될 문자열 형식을 패턴으로 지정하는 출력 구문
+		
+		int iNum1 = 10;
+		int iNum2 = 5;
+		int iNum3 = 2;
+		
+		
+		// 10 + 5 = 15
+		System.out.println("10 + 5 = " + (iNum1 + iNum2));
+		// 길다
+		
+		// System.out.printf("패턴", 패턴에 들어갈 값);
+		System.out.printf("%d + %d = %d\n", iNum1, iNum2, iNum1 + iNum2);
+		// printf도 print와 같이 개행이 되지 않기 때문에
+		// \n 같은 개행 문자(escape)를 사용하여 다음줄로 개행가능
+		// 10 + 10 * 5 / 2 = 35
+		System.out.printf("%d + %d * %d / %d = %d\n", iNum1, iNum1, iNum2, iNum3, iNum1 + iNum1 * iNum2 / iNum3);
+		System.out.println();
+		
+		
+		int iNum4 = 3;
+		System.out.printf("%5d\n", iNum4);
+		// 5칸 공간 확보 후 오른쪽 정렬
+		System.out.printf("%-5d\n" , iNum4);
+		// 왼쪽 정렬은 음수로 표시
+		System.out.println();
+		
+		
+		// 소수점 자리 제어
+		System.out.printf("%f\n", 2.5); // 2,500000
+		System.out.printf("%.2f\n", 2.5); // 소수점 둘째 자리까지만 나타내기
+		System.out.printf("%.0f\n", 2.5); // 소수점 표시 하지 않겠다 but 반올림됨
+		System.out.println();
+		
+		// escape (이스케이프 == 탈출) 문자 : 일반 문자가 아닌, 특수 문자 표현 할 때 한다.
+		System.out.println("\"Read this\"");
+		System.out.println("\\"); // 백슬래시(역슬래시) 출력법
+		System.out.println("\"I'm Lee HyunKyung\"");
+		System.out.println("a\tb\tc\td"); // tap 키
+		System.out.println();
+		System.out.println("a\nb\nc");
+		System.out.println();
+		
+		
+		// 유니코드 이스케이프
+		// 65 => A
+		System.out.println("\u0041");
+		
+		
+		
+		
+	}
+
+}
---
+++ src/edu/kh/variable/ex2/ScannerExample1.java
@@ -0,0 +1,83 @@
+package edu.kh.variable.ex2;
+
+import java.util.Scanner;
+
+public class ScannerExample1 {
+
+	public static void main(String[] args) {
+		
+		// Scanner : 프로그램 실행 중 키보드 입력을 받을 수 있게 하는 것.
+		
+		// Scanner 생성
+		// -> 프로그램 안에 스캐너라는 기계를 만든 것
+		Scanner sc = new Scanner(System.in);
+		// import를 안하면 스캐너 설계도가 없어서 오류가 뜨는데 그냥 new 해도 오류 목록에 떠서 클릭하면 완료!
+		// sc--> 스캐너라는 기능이 들어 있는 변수
+		
+		
+		
+		/*
+		System.out.println("값을 입력하시오~: ");
+		int input1 = sc.nextInt(); // 입력 받은 정수를 input1 이라는 변수에 대입 시킨다는 뜻
+		// nextInt() : 다읍 입력된 정수를 읽어옴 (키보드로 입력된 정수를 읽어옴)
+		System.out.println(input1);
+		System.out.println();
+		
+		System.out.println("실수만 입력해: ");
+		double input2 = sc.nextDouble();
+		System.out.println(input2);
+		*/
+		
+		// 안녕 반가워! 밥 먹을래?
+		
+		/*
+		System.out.println("입력1: ");
+		String input3 = sc.next(); // next() : 다음 입력 된 한 단어를 읽어옴.
+		System.out.println(input3);
+		
+		
+		System.out.println("입력2: ");
+		String input4 = sc.next(); 
+		System.out.println(input4);
+		*/
+		
+		
+		
+		/*
+		System.out.println("인사를 하세요: ");
+		String input10 = sc.nextLine();
+		System.out.println(input10);
+		
+		System.out.println("주소를 입력 하시오: ");
+		String input11 = sc.nextLine();
+		System.out.println(input11);
+		*/
+		
+		/*
+		System.out.print("안녕하세요 점심시간이에요?: ");
+		String input100 = sc.next();
+		System.out.println(input100);
+		
+		System.out.print("뭐 드시고 시프세요?: ");
+		String input101 = input100 + " " + sc.next();
+		System.out.println(input101);
+		*/
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
